7fbd24be444dd30af19f146e1e7b78bdbae67574
Bert Robberechts <bert.robberechts@ed.ac.uk>
2018-12-03 16:27:20 +0000

    #1 return list of notebooks with assignment listing. keep track of notebooks for listing in the database

--- nbexchange/orm.py
+++ nbexchange/orm.py
@@ -386,7 +386,6 @@ class Notebook(Base):
     def __repr__(self):
         return "Notebook<{}/{}>".format(self.assignment.name, self.name)
 
-
 ### ref: https://docs.sqlalchemy.org/en/latest/orm/basic_relationships.html
 
 

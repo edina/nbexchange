f830f76a6cbdc800ec59d285718dc85eb3006f02
Bert Robberechts <bert.robberechts@ed.ac.uk>
2020-03-13 15:27:34 +0000

    WIP feedback

--- nbexchange/plugin/release_feedback.py
+++ nbexchange/plugin/release_feedback.py
@@ -3,6 +3,7 @@ import io
 import json
 import nbgrader.exchange.abc as abc
 import os
+import tempfile
 
 from urllib.parse import quote_plus
 
@@ -32,4 +33,28 @@ class ExchangeReleaseFeedback(abc.ExchangeReleaseFeedback, Exchange):
         pass
 
     def copy_files(self):
-        pass
+        the_file = tempfile.TemporaryFile()
+        self.upload(tempfile)
+
+    def upload(self, file):
+        files = {"feedback" ("feedback.html", file)}
+
+        url = f"feedback?course_id={quote_plus(self.course_id)}&assignment_id={quote_plus(self.coursedir.assignment_id)}"
+
+        r = self.api_request(
+            url,
+            method="POST",
+            files=files,
+        )
+
+        self.log.debug(f"Got back {r.status_code} after feedback upload")
+
+        try:
+            data = r.json()
+        except json.decoder.JSONDecodeError:
+            self.fail(r.text)
+
+        if not data["success"]:
+            self.fail(data["note"])
+
+        self.log.info("Successfully uploaded feedback for assignment.")

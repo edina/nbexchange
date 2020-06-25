fcaa4d48e83830b31dc392b5c0c784f14655d458
Ludvig Kihlman <lkihlman@lkihlman.net>
2020-06-25 12:03:42 +0100

    now with black

--- nbexchange/alembic/versions/2805bf7747e5_change_feedback_timestamp_column_type_.py
+++ nbexchange/alembic/versions/2805bf7747e5_change_feedback_timestamp_column_type_.py
@@ -17,20 +17,24 @@ depends_on = None
 def upgrade():
     with op.batch_alter_table("feedback") as batch_op:
         batch_op.alter_column(
-            "timestamp", existing_type=sa.VARCHAR(length=12), type_=sa.DateTime(timezone=True)
+            "timestamp",
+            existing_type=sa.VARCHAR(length=12),
+            type_=sa.DateTime(timezone=True),
         )
     with op.batch_alter_table("action") as batch_op:
         batch_op.alter_column(
-            "timestamp", existing_type=sa.DateTime, type_=sa.DateTime(timezone=True),
+            "timestamp", existing_type=sa.DateTime, type_=sa.DateTime(timezone=True)
         )
 
 
 def downgrade():
     with op.batch_alter_table("feedback") as batch_op:
         batch_op.alter_column(
-            "timestamp", existing_type=sa.DateTime(timezone=True), type_=sa.VARCHAR(length=12),
+            "timestamp",
+            existing_type=sa.DateTime(timezone=True),
+            type_=sa.VARCHAR(length=12),
         )
     with op.batch_alter_table("action") as batch_op:
         batch_op.alter_column(
-            "timestamp", existing_type=sa.DateTime(timezone=True), type_=sa.DateTime,
+            "timestamp", existing_type=sa.DateTime(timezone=True), type_=sa.DateTime
         )

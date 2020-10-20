469439f15c7ece5c2f8154f8b8dc803629678b46
Bert Robberechts <bert.robberechts@ed.ac.uk>
2020-10-20 11:49:33 +0100

    Get last active assignment, instead of the first assignment, for a given assignment code

--- nbexchange/handlers/feedback.py
+++ nbexchange/handlers/feedback.py
@@ -59,6 +59,8 @@ class FeedbackHandler(BaseHandler):
             assignment = (
                 session.query(nbexchange.models.Assignment)
                 .filter_by(assignment_code=assignment_id)
+                .filter_by(active=True)
+                .order_by(nbexchange.models.Assignment.id.desc())
                 .first()
             )
             self.log.info(assignment)

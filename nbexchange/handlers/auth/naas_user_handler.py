47c0e09192116f668d036aa460f4f7859fbe5999
Andrew Seales <andrew.seales@ed.ac.uk>
2021-01-15 15:26:35 +0000

    Fix username due to change in auth (#52)
    The /api endpoint in noteable api returns usernames in the form 1-xyz.
    The JWT stores usernames in the form 1_xyz. Sadly all the databases and
    filesystems use the 1-xyz form.

--- nbexchange/handlers/auth/naas_user_handler.py
+++ nbexchange/handlers/auth/naas_user_handler.py
@@ -24,8 +24,18 @@ class NaasUserHandler(BaseUserHandler):
         encoded = cookies["noteable_auth"]
         result = jwt.decode(encoded, self.jwt_key, algorithms=["HS256"])
 
+        # TODO this _ to - transformation is unfortunate but the alternatives are also bad
+        # Due to changes in the API in aug/sept 2020 the username was transformed for the UI to appear
+        # as 1-xyz instead of 1_xyz. This was due to K8S only supporting DNS compatible characters for some reasources
+        # which _ isn't. The other nice benefit was to get rid of %2F in places. Unfortunately nbexchange used this
+        # same API and its username format was changed at the same time.
+        # The username is used in the path to user assignment submissions and is recorded in the nbexchange database
+        # and on the NFS filesystem. Changing this back would require these usernames are reformatted from their
+        # 1-xyz format back to 1_xyz
+        transformed_username = result["username"].replace("_", "-", 1)
+
         return {
-            "name": result["username"],
+            "name": transformed_username,
             "course_id": result["n_cid"],
             "course_title": result["n_cnm"],
             "course_role": result["n_rl"],

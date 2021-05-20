dcca69564dee2b2cd5ef65cc1201cb2f95cbcb06
Ian Stuart <ian.stuart@ed.ac.uk>
2021-05-20 14:26:28 +0100

    lint the test_handlers_auth file

--- nbexchange/tests/test_handlers_auth.py
+++ nbexchange/tests/test_handlers_auth.py
@@ -1,13 +1,13 @@
 import logging
-import pytest
-
 from abc import ABCMeta
+
+import pytest
 
 from nbexchange.handlers.auth.user_handler import BaseUserHandler
 
-
 logger = logging.getLogger(__file__)
 logger.setLevel(logging.ERROR)
+
 
 @pytest.mark.gen_test
 def test_base_abstract_class(app):

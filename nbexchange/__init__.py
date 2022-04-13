62e0fdc9a60a48e14a2a0261b8bce0084024849d
Ian Stuart <ian.stuart@ed.ac.uk>
2022-04-13 14:08:15 +0100

    Lint nbexchange/__init__.py

--- nbexchange/__init__.py
+++ nbexchange/__init__.py
@@ -10,14 +10,13 @@ VERSION = (1, 2, 6)
 __version__ = ".".join(map(str, VERSION))
 
 
-
 def _jupyter_nbextension_paths():
     paths = [
         dict(
             section="notebook",
-            src=os.path.join('nbextensions', 'nbexchange_history'),
+            src=os.path.join("nbextensions", "nbexchange_history"),
             dest="nbexchange_history",
-            require="nbexchange_history/main"
+            require="nbexchange_history/main",
         ),
     ]
 
@@ -29,7 +28,7 @@ def _jupyter_server_extension_paths():
         dict(module="nbexchange.server_extensions.exchange_history"),
     ]
 
-    if sys.platform != 'win32':
+    if sys.platform != "win32":
         paths.append(dict(module="nbexchange.server_extensions.exchange_history"))
 
     return paths

0ffbfeb6e49527537b8b1aa40f0435d9ab4c1617
jnewton3 <jnewton3@ed.ac.uk>
2022-08-18 14:07:28 +0100

    Updated clear list function

--- nbexchange/nbextensions/nbexchange_history/nbexchange_history.js
+++ nbexchange/nbextensions/nbexchange_history/nbexchange_history.js
@@ -38,7 +38,7 @@ define([
 
     // Remove all 'div' elements under the #history_list element
     CourseList.prototype.clear_list = function (loading) {
-         $('#history_list').children('div').remove
+         $('#history_list').children('div').remove()
          var foo = this.history_root_selector;
     };
 

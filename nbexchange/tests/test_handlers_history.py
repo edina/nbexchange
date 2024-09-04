2d2fab60e0d7cfe9763de4e56b255d8b9e33bfcd
Callum Fenna-Roberts <callum.fenna-roberts@ed.ac.uk>
2024-09-04 21:58:02 +0100

    Add new history handler which returns assignments and actions based on action type, assignment_id and/or course_code

---
+++ nbexchange/tests/test_handlers_history.py
@@ -0,0 +1,262 @@
+import datetime
+import logging
+
+import pytest
+from mock import patch
+
+from nbexchange.handlers.base import BaseHandler
+from nbexchange.tests.test_handlers_base import BaseTestHandlers
+from nbexchange.tests.utils import async_requests, user_kiz_instructor
+
+from nbexchange.tests.test_orm import user_johaannes, assignment_tree, user_kaylee
+
+from nbexchange.models.actions import Action, AssignmentActions
+
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.ERROR)
+
+
+class TestHandlersHistory(BaseTestHandlers):
+
+    # require authenticated user (404 because the bounce to login fails)
+    @pytest.mark.gen_test
+    def test_history_must_be_authenticated(self, app):
+        r = yield async_requests.get(app.url + "/history")
+        assert r.status_code == 403
+
+    @pytest.mark.gen_test
+    def test_history_invalid_action_param(self, app):
+        with patch.object(BaseHandler, "get_current_user", return_value=user_kiz_instructor):
+            r = yield async_requests.get(app.url + "/history?action=foo")
+        assert r.status_code == 200
+        response_data = r.json()
+        assert response_data["success"] is False
+        assert response_data["note"] == "foo is not a valid assignment action."
+
+    @pytest.mark.gen_test
+    def test_history_no_action_param(self, app, action_submitted):
+        with patch.object(BaseHandler, "get_current_user", return_value=user_kiz_instructor):
+            r = yield async_requests.get(app.url + "/history")
+        assert r.status_code == 200
+        response_data = r.json()
+        assert response_data["success"] is True
+        assert "value" in response_data
+        assert response_data["value"] == [
+            {
+                "role": {"Instructor": 1},
+                "user_id": {"3": 1},
+                "assignments": [
+                    {
+                        "assignment_id": 1,
+                        "assignment_code": "tree 1",
+                        "actions": [
+                            {
+                                "action": "AssignmentActions.submitted",
+                                "timestamp": "1970-01-02 10:10:00.000000 ",
+                                "user": "Johaannes",
+                            }
+                        ],
+                        "action_summary": {"submitted": 1},
+                    }
+                ],
+                "isInstructor": True,
+                "course_id": 1,
+                "course_code": "course_2",
+                "course_title": "A title",
+            }
+        ]
+
+    # Instructor should be able to see feedback_released action by another instructor
+    @pytest.mark.gen_test
+    def test_history_feedback_released(self, app, action_feedback_released):
+        with patch.object(BaseHandler, "get_current_user", return_value=user_kiz_instructor):
+            r = yield async_requests.get(app.url + "/history")
+        assert r.status_code == 200
+        response_data = r.json()
+        print(response_data)
+        assert response_data["success"] is True
+        assert "value" in response_data
+        assert response_data["value"] == [
+            {
+                "role": {"Instructor": 1},
+                "user_id": {"3": 1},
+                "assignments": [
+                    {
+                        "assignment_id": 1,
+                        "assignment_code": "tree 1",
+                        "actions": [
+                            {
+                                "action": "AssignmentActions.submitted",
+                                "timestamp": "1970-01-02 10:10:00.000000 ",
+                                "user": "Johaannes",
+                            },
+                            {
+                                "action": "AssignmentActions.feedback_released",
+                                "timestamp": "1970-01-31 10:10:00.000000 ",
+                                "user": "kaylee",
+                            },
+                        ],
+                        "action_summary": {"submitted": 1, "feedback_released": 1},
+                    }
+                ],
+                "isInstructor": True,
+                "course_id": 1,
+                "course_code": "course_2",
+                "course_title": "A title",
+            }
+        ]
+
+    @pytest.mark.gen_test
+    def test_history_action_feedback_released(self, app):
+        with patch.object(BaseHandler, "get_current_user", return_value=user_kiz_instructor):
+            r = yield async_requests.get(app.url + "/history?action=feedback_released")
+        assert r.status_code == 200
+        response_data = r.json()
+        print(response_data)
+        assert response_data["success"] is True
+        assert "value" in response_data
+        assert response_data["value"] == [
+            {
+                "role": {"Instructor": 1},
+                "user_id": {"3": 1},
+                "assignments": [
+                    {
+                        "assignment_id": 1,
+                        "assignment_code": "tree 1",
+                        "actions": [
+                            {
+                                "action": "AssignmentActions.feedback_released",
+                                "timestamp": "1970-01-31 10:10:00.000000 ",
+                                "user": "kaylee",
+                            },
+                        ],
+                        "action_summary": {"feedback_released": 1},
+                    }
+                ],
+                "isInstructor": True,
+                "course_id": 1,
+                "course_code": "course_2",
+                "course_title": "A title",
+            }
+        ]
+
+    @pytest.mark.gen_test
+    def test_history_filter_by_assignment_id(self, app):
+        with patch.object(BaseHandler, "get_current_user", return_value=user_kiz_instructor):
+            r = yield async_requests.get(app.url + "/history?assignment_id=987654321")
+        assert r.status_code == 200
+        response_data = r.json()
+        assert response_data["success"] is True
+        assert "value" in response_data
+        assert response_data["value"] == [
+            {
+                "role": {"Instructor": 1},
+                "user_id": {"3": 1},
+                "assignments": [],
+                "isInstructor": True,
+                "course_id": 1,
+                "course_code": "course_2",
+                "course_title": "A title",
+            }
+        ]
+
+    @pytest.mark.gen_test
+    def test_history_filter_by_course_code_not_exist(self, app):
+        with patch.object(BaseHandler, "get_current_user", return_value=user_kiz_instructor):
+            r = yield async_requests.get(app.url + "/history?course_code=abcdefghij123456")
+        assert r.status_code == 200
+        response_data = r.json()
+        assert response_data["success"] is True
+        assert "value" in response_data
+        assert response_data["value"] == []
+
+    @pytest.mark.gen_test
+    def test_history_filter_by_course_code_exists(self, app):
+        with patch.object(BaseHandler, "get_current_user", return_value=user_kiz_instructor):
+            r = yield async_requests.get(app.url + "/history?course_code=course_2")
+        assert r.status_code == 200
+        response_data = r.json()
+        print(response_data)
+        assert response_data["success"] is True
+        assert "value" in response_data
+        assert response_data["value"] == [
+            {
+                "role": {"Instructor": 1},
+                "user_id": {"3": 1},
+                "assignments": [
+                    {
+                        "assignment_id": 1,
+                        "assignment_code": "tree 1",
+                        "actions": [
+                            {
+                                "action": "AssignmentActions.submitted",
+                                "timestamp": "1970-01-02 10:10:00.000000 ",
+                                "user": "Johaannes",
+                            },
+                            {
+                                "action": "AssignmentActions.feedback_released",
+                                "timestamp": "1970-01-31 10:10:00.000000 ",
+                                "user": "kaylee",
+                            },
+                        ],
+                        "action_summary": {"submitted": 1, "feedback_released": 1},
+                    }
+                ],
+                "isInstructor": True,
+                "course_id": 1,
+                "course_code": "course_2",
+                "course_title": "A title",
+            }
+        ]
+
+
+@pytest.fixture
+def action_submitted(db, user_johaannes, assignment_tree):
+    orm_action = Action(
+        user_id=user_johaannes.id,
+        assignment_id=assignment_tree.id,
+        action=AssignmentActions.submitted,
+        location="/some/random/path/to/a/file.tzg",
+        timestamp=datetime.datetime(1970, 1, 2, 10, 10),
+    )
+    db.add(orm_action)
+    db.commit()
+
+
+@pytest.fixture
+def action_fetched(db, user_johaannes, assignment_tree):
+    orm_action = Action(
+        user_id=user_johaannes.id,
+        assignment_id=assignment_tree.id,
+        action=AssignmentActions.fetched,
+        location="/some/random/path/to/a/file.tzg",
+        timestamp=datetime.datetime(1970, 1, 1, 10, 10),
+    )
+    db.add(orm_action)
+    db.commit()
+
+
+@pytest.fixture
+def action_collected(db, assignment_tree, user_kaylee):
+    orm_action = Action(
+        user_id=user_kaylee.id,
+        assignment_id=assignment_tree.id,
+        action=AssignmentActions.collected,
+        location="/some/random/path/to/a/file.tzg",
+        timestamp=datetime.datetime(1970, 1, 30, 10, 10),
+    )
+    db.add(orm_action)
+    db.commit()
+
+
+@pytest.fixture
+def action_feedback_released(db, assignment_tree, user_kaylee):
+    orm_action = Action(
+        user_id=user_kaylee.id,
+        assignment_id=assignment_tree.id,
+        action=AssignmentActions.feedback_released,
+        location="/some/random/path/to/a/file.tzg",
+        timestamp=datetime.datetime(1970, 1, 31, 10, 10),
+    )
+    db.add(orm_action)
+    db.commit()

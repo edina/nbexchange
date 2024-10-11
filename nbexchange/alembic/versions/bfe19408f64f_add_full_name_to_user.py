0d510872e821ef503ea6466fca4eaaf29bb54a71
Ian Stuart <Ian.Stuart@ed.ac.uk>
2024-10-11 15:47:25 +0100

    un-fsck ian's mis-revision linking

--- nbexchange/alembic/versions/bfe19408f64f_add_full_name_to_user.py
+++ nbexchange/alembic/versions/bfe19408f64f_add_full_name_to_user.py
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "bfe19408f64f"
-down_revision = "2540572282f2"
+down_revision = "f3345539f08d"
 branch_labels = None
 depends_on = None
 

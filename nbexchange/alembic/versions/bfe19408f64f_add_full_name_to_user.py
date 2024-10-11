97a3db0d6ec5b99979d27b34e16eadef9fc571a2
Ian Stuart <Ian.Stuart@ed.ac.uk>
2024-10-11 15:38:22 +0100

    Sort out revisions again

--- nbexchange/alembic/versions/bfe19408f64f_add_full_name_to_user.py
+++ nbexchange/alembic/versions/bfe19408f64f_add_full_name_to_user.py
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "bfe19408f64f"
-down_revision = "f3345539f08d"
+down_revision = "2540572282f2"
 branch_labels = None
 depends_on = None
 

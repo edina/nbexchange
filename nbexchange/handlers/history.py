6833f3e0b5e6e7983c9eeee5165c360255fc00b7
Ian Stuart <Ian.Stuart@ed.ac.uk>
2026-09-04 15:12:02 +0100

    Update the history handler to be significantly faster

--- nbexchange/handlers/history.py
+++ nbexchange/handlers/history.py
@@ -1,10 +1,9 @@
+from dateutil.parser import parse as date_util_parse
+from sqlalchemy import text
 from tornado import web
 
-import nbexchange.models
-import nbexchange.models.subscriptions
 from nbexchange.database import scoped_session
 from nbexchange.handlers.base import BaseHandler, authenticated
-from nbexchange.models.action_history_view import ActionHistoryView
 from nbexchange.models.actions import AssignmentActions
 
 """
@@ -43,8 +42,14 @@ class History(BaseHandler):
             course_id: Int,
             course_code: Str,
             course_title: Str,
-            role: [Str, Str, ..],
-            user_id: [Str, Str, ..],
+            role: {
+                $role: str,
+                $count: Int
+            },
+            user_id: {
+                $user_id: Str,
+                $count: Int
+            },
             isInstructor: Bool,
             assignments: [
                 {
@@ -99,50 +104,86 @@ class History(BaseHandler):
         this_user = self.nbex_user
         self.log.debug(f"History authenticated User: {this_user.get('name')}")
 
+        # This gets a bit complicated: we need to separate the role from the courses & actions because
+        # when a user has both roles on a course, a single query create a duplicate row for each role.
+        # We avoid this by getting the roles separately, then merge them into the final model.
+        params = {"user_id": this_user["id"]}
+
         with scoped_session() as session:
-
-            rows = session.query(nbexchange.models.Subscription).all()
-            self.log.info(f"subscriptions: {len(rows)}\n {rows}")
-            rows = session.query(nbexchange.models.Course).all()
-            self.log.info(f"courses: {len(rows)}\n {rows}")
-            rows = session.query(nbexchange.models.Assignment).all()
-            self.log.info(f"assignments: {len(rows)}\n {rows}")
-            rows = session.query(nbexchange.models.Action).all()
-            self.log.info(f"actions: {len(rows)}\n {rows}")
-            rows = session.query(ActionHistoryView).all()
-            self.log.info(f"view: {len(rows)}\n {rows}")
-
-            query = session.query(ActionHistoryView).filter(ActionHistoryView.viewer_user_id == this_user["id"])
+            query = """
+            SELECT
+              c.id as course_id,
+              c.course_code,
+              c.course_title,
+              ass.id as assignment_id,
+              ass.assignment_code,
+              act.id as action_id,
+              act.user_id,
+              u.name,
+              act.action,
+              act.location,
+              act.timestamp
+            FROM course c, assignment ass, action act, "user" u
+            WHERE act.user_id=u.id
+              and act.assignment_id=ass.id
+              and ass.course_id=c.id
+              and ass.active=True
+              and c.id in (
+                SELECT s.course_id
+                FROM subscription s
+                WHERE s.user_id=:user_id
+                )
+            """
 
             if course_id_param and course_id_param != "moot":
-                query = query.filter(ActionHistoryView.course_code == course_id_param)
+                query += " AND c.course_code = :course_code"
+                params["course_code"] = course_id_param
 
             if action_param:
-                query = query.filter(ActionHistoryView.action == action_param)
-            self.log.info(f"History: query: {query}")
-            rows = query.all()
+                query += " AND act.action=:action"
+                params["action"] = action_param
+
+            query += " ORDER BY c.course_code, ass.assignment_code, act.id ASC"
+            rows = session.execute(text(query), params).all()
+
             self.log.info(f"History: {len(rows)} rows returned for user {this_user['name']}")
-            self.log.info(rows)
+
+            roles_query = text("SELECT s.course_id, s.role FROM subscription s WHERE s.user_id=:this_user_id")
+            params["this_user_id"] = this_user["id"]
+            roles_rows = session.execute(roles_query, params).all()
+            course_roles = {}
+            for role_row in roles_rows:
+                if role_row.course_id not in course_roles:
+                    course_roles[role_row.course_id] = {}
+                if role_row.role not in course_roles[role_row.course_id]:
+                    course_roles[role_row.course_id][role_row.role] = 1
+
             models = {}
             for row in rows:
-                if row.viewer_role == "Instructor":
+                self.log.debug(f"History: row: {row}")
+                course_is_instructor = "Instructor" in course_roles.get(row.course_id, {})
+                if course_is_instructor:
                     pass
-                elif row.action != AssignmentActions.released and row.actor_user_id != this_user["id"]:
+                elif row.action != "released" and row.user_id != this_user["id"]:
+                    self.log.debug(
+                        f"History: skipping : {row.action} != 'released' and user {row.user_id} != {this_user['id']}"
+                    )
                     continue
 
+                # set up the top-level course model if it doesn't exist yet.
+                # `user_id` is a dict to note who the querant is... I never promised great code.
                 if row.course_id not in models:
                     models[row.course_id] = {
-                        "role": {row.viewer_role: 1},
-                        "user_id": {row.viewer_user_id: 1},
+                        "user_id": {this_user["id"]: 1},
+                        "role": course_roles.get(row.course_id, {}),
+                        "isInstructor": course_is_instructor,
                         "assignments": {},
-                        "isInstructor": row.viewer_role == "Instructor",
                         "course_id": row.course_id,
                         "course_code": row.course_code,
                         "course_title": row.course_title,
                     }
 
-                models[row.course_id]["role"][row.viewer_role] = 1
-
+                # Now set up the assignment sub-models if they doesn't exist already.
                 if row.assignment_id not in models[row.course_id]["assignments"]:
                     models[row.course_id]["assignments"][row.assignment_id] = {
                         "assignment_id": row.assignment_id,
@@ -151,16 +192,19 @@ class History(BaseHandler):
                         "action_summary": {},
                     }
 
-                action_string = str(row.action).replace("AssignmentActions.", "")
-                if action_string not in models[row.course_id]["assignments"][row.assignment_id]["action_summary"]:
-                    models[row.course_id]["assignments"][row.assignment_id]["action_summary"][action_string] = 0
-                models[row.course_id]["assignments"][row.assignment_id]["action_summary"][action_string] += 1
+                if row.action not in models[row.course_id]["assignments"][row.assignment_id]["action_summary"]:
+                    models[row.course_id]["assignments"][row.assignment_id]["action_summary"][row.action] = 0
+                models[row.course_id]["assignments"][row.assignment_id]["action_summary"][row.action] += 1
 
                 this_action = {
-                    "action": str(row.action),
+                    # because the jlab extensions expects this format... :sigh:
+                    "action": "AssignmentActions." + str(row.action),
                     "path": row.location,
-                    "timestamp": self.check_timezone(row.timestamp).strftime(self.timestamp_format),
-                    "user": row.actor_name,
+                    # timestamp comes through as a string.
+                    "timestamp": self.check_timezone(date_util_parse(str(row.timestamp))).strftime(
+                        self.timestamp_format
+                    ),
+                    "user": row.name,
                 }
                 models[row.course_id]["assignments"][row.assignment_id]["actions"].append(this_action)
 
